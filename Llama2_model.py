89f43f505dd0fa5888c9177be41c2011b055ce89
Тимур Комолов <komolov.timurka@mail.ru>
2024-08-22 15:37:24 +0300

    added vk-bot service

---
+++ Llama2_model.py
@@ -0,0 +1,86 @@
+from llama_cpp import Llama
+from tqdm import tqdm
+import os
+
+SYSTEM_PROMPT = "Ты — переводчик. Ты переводишь текст с русского, на текст, как будто он был переведён с китайского. Пример: 'Меня зовут Иван, живу в России и я работаю в шахте. Читал труды китайской партии, и мне понравилось.' -> 'Я простой русский рабочий Иван, работать шахта, жить Россия. Читать книга Китай партия, много нравиться. "
+SYSTEM_TOKEN = 1788
+USER_TOKEN = 1404
+BOT_TOKEN = 9225
+LINEBREAK_TOKEN = 13
+
+top_k = 40
+top_p = 0.5
+temperature = 0.01
+repeat_penalty = 1.1
+
+ROLE_TOKENS = {
+    "user": USER_TOKEN,
+    "bot": BOT_TOKEN,
+    "system": SYSTEM_TOKEN
+}
+
+
+def get_message_tokens(model, role, content):
+    message_tokens = model.tokenize(content.encode("utf-8"))
+    message_tokens.insert(1, ROLE_TOKENS[role])
+    message_tokens.insert(2, LINEBREAK_TOKEN)
+    message_tokens.append(model.token_eos())
+    return message_tokens
+
+
+def get_system_tokens(model):
+    system_message = {
+        "role": "system",
+        "content": SYSTEM_PROMPT
+    }
+    return get_message_tokens(model, **system_message)
+
+
+def get_prompt(question):
+    return f"""
+    Текст, который нужно перевести: {question}
+    Переведи с русского так, как будто этот текст был переведён с китайского в переводчике."""
+
+
+def chat_saiga(message, model):
+    message = get_prompt(message)
+    system_tokens = get_system_tokens(model)
+    tokens = system_tokens
+    # model.eval(tokens)
+
+    message_tokens = get_message_tokens(model=model, role="user", content=message)
+    role_tokens = [model.token_bos(), BOT_TOKEN, LINEBREAK_TOKEN]
+    tokens += message_tokens + role_tokens
+    # print(tokens)
+    # detokenize = model.detokenize(tokens)
+    # print(model.tokenize(full_prompt))
+    generator = model.generate(
+        tokens,
+        top_k=top_k,
+        top_p=top_p,
+        temp=temperature,
+        repeat_penalty=repeat_penalty,
+        reset=True
+    )
+    # print(len([token for token in generator]))
+
+    result_list = []
+
+    for token in generator:
+        token_str = model.detokenize([token]).decode("utf-8", errors="ignore")
+        tokens.append(token)
+        if token == model.token_eos():
+            break
+        print(token_str, end="", flush=True)
+        result_list.append(token_str)
+    return ''.join(result_list)
+
+model_path = 'model-100step.gguf'
+full_path = model_path
+n_ctx = 3096
+
+model = Llama(
+    model_path=full_path,
+    n_ctx=n_ctx,
+    n_gpu_layers=-1
+)
---
+++ llm_bot.py
@@ -0,0 +1,52 @@
+import vk_api
+from private_api import PRIVATE_API
+from vk_api.longpoll import VkLongPoll, VkEventType
+import time
+import requests
+from vk_api.utils import get_random_id
+from Llama2_model import chat_saiga, model
+import thread
+
+
+def send_message(user_id: int, msg: str, stiker=None, attach=None) -> None:
+    try:
+        vk.messages.send(
+            user_id=user_id,
+            random_id=get_random_id(),
+            message=msg,
+            sticker_id=stiker,
+            attachment=attach
+        )
+    except BaseException as ex:
+        print(ex)
+        return
+
+
+def async_generate(user_id: int, message: str, event):
+    vk.messages.setActivity(peer_id=event.peer_id, type='typing')
+    response = chat_saiga(message, model)
+    send_message(user_id, response)
+
+
+def main():
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            user_id = event.user_id
+            if event.text:
+                thread.start_new_thread(async_generate, (user_id, event.text, event))
+            else:
+                send_message(user_id, "Отсутствие текста")
+
+
+if __name__ == "__main__":
+    vk_session = vk_api.VkApi(token=PRIVATE_API)
+    vk = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    while True:
+        try:
+            main()
+        except requests.exceptions.ReadTimeout:
+            print("read-timeout")
+            time.sleep(600)
+        except Exception as ex:
+            print(ex)
