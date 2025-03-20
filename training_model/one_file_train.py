bc0116440568a9693fdde23a4728330cf8148c68
Timur Komolov <komolov.timurka@mail.ru>
2025-03-20 17:21:50 +0300

    Make some fixes for q8

--- training_model/one_file_train.py
+++ training_model/one_file_train.py
@@ -139,17 +139,17 @@ def train(cfg: DictConfig):
         if isinstance(cfg.model.torch_dtype, str)
         else cfg.model.torch_dtype
     )
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype=torch_dtype,
-        bnb_4bit_use_double_quant=True,
-    )
     # bnb_config = BitsAndBytesConfig(
-    #     load_in_8bit=True,  # Основное изменение
-    #     llm_int8_threshold=6.0,  # Опционально: порог для квантизации
-    #     torch_dtype=torch_dtype,  # Сохраняем указание типа данных
+    #     load_in_4bit=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_compute_dtype=torch_dtype,
+    #     bnb_4bit_use_double_quant=True,
     # )
+    bnb_config = BitsAndBytesConfig(
+        load_in_8bit=True,  # Основное изменение
+        llm_int8_threshold=6.0,  # Опционально: порог для квантизации
+        torch_dtype=torch_dtype,  # Сохраняем указание типа данных
+    )
     model = AutoModelForCausalLM.from_pretrained(
         cfg.model.model_name,
         quantization_config=bnb_config,
